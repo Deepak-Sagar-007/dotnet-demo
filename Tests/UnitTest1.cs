bd5b46179a52c242f561504596abb504d591cc6a
Deepak <deepaksagar924@gmail.com>
2026-07-14 10:28:18 +0530

    yml file updated

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+public class TaskManagerTests
+{
+    [Fact]
+    public void PendingTasks_ShouldBeCorrect()
+    {
+        var manager = new TaskManager();
+        Assert.Equal(2, manager.GetPendingTaskCount());
+    }
+
+    [Fact]
+    public void CompletedTasks_ShouldBeCorrect()
+    {
+        var manager = new TaskManager();
+        Assert.Equal(1, manager.GetCompletedTaskCount());
+    }
+}
